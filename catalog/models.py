57545eb1c1b6e0101ffb968631940532bd9e9b77
Steve Cendejas <scendejasm@gmail.com>
2024-06-21 10:40:49 -0700

    Added Language model with fkey_ref on book model

--- catalog/models.py
+++ catalog/models.py
@@ -56,6 +56,12 @@ class Book(models.Model):
     genre = models.ManyToManyField(
             Genre, help_text="Select a genre for this book")
 
+    #Add a foreign key relation to a Language model 
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+
+    class Meta: 
+        ordering = ['title', 'author']
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
@@ -115,3 +121,28 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
+
+class Language(models.Model):
+    """Model representing a language a book can be written in."""
+    name = models.CharField(max_length=200,
+                            unique=True,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+
+    def get_absolute_url(self):
+        """Returns the Url to access a particular language instance."""
+        return reverse('language-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Language object (in Admin site etc.)"""
+        return self.name
+
+    class Meta: 
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='language_name_case_insensitive_unique',
+                violation_error_message = "Language alrady exists (case insensitive match)"
+            ),
+        ]
