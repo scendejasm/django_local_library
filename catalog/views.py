a9f3b013abfbc80236218b2c10420aa9919c845b
Steve Cendejas <scendejasm@gmail.com>
2024-06-21 17:30:00 -0700

    implement view and template for home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
+from .models import Book, Author, BookInstance, Genre
+
 # Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Genrate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default. 
+    num_authors = Author.objects.count()
+
+    num_genres = Genre.objects.count()
+
+    num_titles_with_specific_word = Book.objects.filter(title__contains='the').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_titles_with_specific_word': num_titles_with_specific_word
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'catalog/index.html', context=context)
